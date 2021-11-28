c4ee7631e584803d7fed351eea5bf4b4c8db724d
Joseph <aelphaeis@live.com>
2021-11-28 15:57:36 -0500

    Create a class to represent the placement.

---
+++ src/main/java/com/cruat/spawn/placement/logos/Placement.java
@@ -0,0 +1,30 @@
+package com.cruat.spawn.placement.logos;
+
+public class Placement {
+
+	private final int id;
+	private final int x;
+	private final int y;
+
+	public Placement(int id, int x, int y) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public String getHumanReadable() {
+		return String.format("{%d, %d, %d}", id, x, y);
+	}
+}
