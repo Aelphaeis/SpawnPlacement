155c312d128ea0dbf2d9a2574c210eb5a130a963
Joseph <aelphaeis@live.com>
2021-11-28 16:49:48 -0500

    Implement program

--- src/main/java/com/cruat/spawn/placement/Program.java
+++ src/main/java/com/cruat/spawn/placement/Program.java
@@ -1,11 +1,30 @@
 package com.cruat.spawn.placement;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import com.cruat.spawn.placement.logos.InputParser;
+import com.cruat.spawn.placement.logos.OutputWriter;
+import com.cruat.spawn.placement.logos.Placement;
+import com.cruat.spawn.placement.logos.PlacementProcessor;
 
 public class Program {
-	private static final Logger logger = LogManager.getLogger();
-	public static void main(String[] args) {
-		logger.info("Hello World");
+
+	public static void main(String[] args) throws IOException {
+		String inFile = args[1];
+		String outFile = args[2];
+
+		int[][] content;
+		try (InputParser parser = new InputParser(new File(inFile))) {
+			content = parser.getContent();
+		}
+		
+		PlacementProcessor processor = new PlacementProcessor(content);
+		List<Placement> placements = processor.process();
+		
+		try(OutputWriter writer = new OutputWriter(new File(outFile))){
+			writer.writePlacements(placements);
+		}
 	}
 }
